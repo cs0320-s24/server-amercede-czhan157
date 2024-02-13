13e6b46e6f934873151f28c76297d952614c7958
carolinebzhang <caroline_b_zhang@brown.edu>
2024-02-13 10:47:01 -0500

    server exception

--- src/main/java/edu/brown/cs/student/main/common/ServerException.java
+++ src/main/java/edu/brown/cs/student/main/common/ServerException.java
@@ -1,9 +1,3 @@
 package edu.brown.cs.student.main.common;
-<<<<<<< Updated upstream
 public class ServerException extends Exception{
-=======
-
-public class ServerException extends Exception{
-
->>>>>>> Stashed changes
 }
