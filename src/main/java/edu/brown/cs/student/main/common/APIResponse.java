a81e47ceb9f706cc51a261a7ac6cef0782935f27
carolinebzhang <caroline_b_zhang@brown.edu>
2024-02-15 20:39:05 -0500

    fixed exceptions and responses

--- src/main/java/edu/brown/cs/student/main/common/APIResponse.java
+++ src/main/java/edu/brown/cs/student/main/common/APIResponse.java
@@ -3,8 +3,8 @@ package edu.brown.cs.student.main.common;
 import com.squareup.moshi.Moshi;
 
 public interface APIResponse {
-    public default String serialize() {
+    default String serialize() {
         Moshi moshi = new Moshi.Builder().build();
-        return moshi.adapter(this.getClass()).toJson(this);
+        return moshi.adapter(APIResponse.class).toJson(this);
     }
 }
